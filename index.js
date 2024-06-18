9983ad057b0a288e2dabf77c6e8cc4c17af9745c
Shanuwa <Shameendurage@gmail.com>
2024-06-18 14:38:09 +0530

    ├✤ 𝑺𝑯𝑨𝑵𝑼𝑾𝑨 𝑴𝑫 ✤┤

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-var __CreateBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
   var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
