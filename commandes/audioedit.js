60668c38dd6dd0056e5c902c85efcd5a25e041a3
Shanuwa <Shameendurage@gmail.com>
2024-06-18 12:08:55 +0530

    ├✤ 𝑺𝑯𝑨𝑵𝑼𝑾𝑨 𝑴𝑫 ✤┤

--- commandes/audioedit.js
+++ commandes/audioedit.js
@@ -6,7 +6,7 @@ const { exec } = require("child_process");
 
 const filename = `${Math.random().toString(36)}`;
 
-zokou (
+Zokou (
     {
         nomCom : 'deep',
         categorie : 'Audio-Edit',
