d00624b41c7d71bcf004203ad1f58db46fb9badc
Shanuwa <Shameendurage@gmail.com>
2024-06-18 13:11:50 +0530

    ├✤ 𝑺𝑯𝑨𝑵𝑼𝑾𝑨 𝑴𝑫 ✤┤

--- framework/index.js
+++ framework/index.js
@@ -1,4 +1,4 @@
-"use Strict";
+"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.genererNomFichier = exports.stick = exports.format = exports.styletext = exports.zJson = exports.getBuffer = exports.reaction = exports.police = exports.zokou = void 0;
 let { zokou } = require("./zokou");
