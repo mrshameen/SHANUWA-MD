3cf56dfa3adab62807a4a293f929fdb39cfff8b1
Shanuwa <Shameendurage@gmail.com>
2024-06-18 12:04:06 +0530

    ├✤ 𝑺𝑯𝑨𝑵𝑼𝑾𝑨 𝑴𝑫 ✤┤

--- commandes/yt-search.js
+++ commandes/yt-search.js
@@ -5,7 +5,7 @@ const yts = require("yt-search");
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 
-Zokou({ nomCom: "yts", categorie: "Search", reaction: "✋" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "yts", categorie: "Search", reaction: "✋" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, arg } = commandeOptions;
   const query = arg.join(" ");
 
