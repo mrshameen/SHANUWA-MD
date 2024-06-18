a474f84ee4ec9889e6e6602fe0a64fc7a825ab1b
Shanuwa <Shameendurage@gmail.com>
2024-06-18 12:57:49 +0530

    ├✤ 𝑺𝑯𝑨𝑵𝑼𝑾𝑨 𝑴𝑫 ✤┤

--- commandes/rank.js
+++ commandes/rank.js
@@ -1,5 +1,5 @@
 
-const {Zokou} = require("../framework/zokou");
+const {zokou} = require("../framework/zokou");
 const conf = require("../set");
 const {getMessagesAndXPByJID,getBottom10Users} = require("../bdd/level");
 
