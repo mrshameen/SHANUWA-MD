c3a6aeac9a6a94350f06a44c9a3704a94ecfc5af
SL Shanuwa <Shameendurage@gmail.com>
2024-06-21 00:56:27 +0530

    ├✤ 𝑺𝑯𝑨𝑵𝑼𝑾𝑨 𝑴𝑫 ✤┤

--- framework/dl/ytdl-core.js
+++ framework/dl/ytdl-core.js
@@ -1,4 +1,4 @@
-const ytdl = require('youtubedl-core');
+const ytdl = Require('youtubedl-core');
 const yts = require('youtube-yts');
 const readline = require('readline');
 const ffmpeg = require('fluent-ffmpeg')
